38e0cb73d187629ea8d0d4e801587b8d38aea9c4
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-04-09 15:04:08 -0400

    Set route and create controllers

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function store(Request $request){
+        return 1;
+    }
+
+
+    public function signin(Request $request){
+        return 13;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix' => 'v1'], function(){
+    Route::resource('meeting', 'MeetingController', [
+        'except' => ['create', 'edit']
+    ]);
+
+    Route::resource('meeting/registration', 'RegistrationController', [
+        'only' => ['store', 'destroy']
+    ]);
+
+    Route::post('user', [
+        'uses' => 'AuthController@store'
+    ]);
+
+    Route::post('user/signin', [
+        'uses' => 'AuthController@signin'
+    ]);
 });
